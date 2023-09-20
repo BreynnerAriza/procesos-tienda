e6e06f569a216725abba5eb89878687a5a6580dc
Breynner Ariza <136642407+BreynnerAriza@users.noreply.github.com>
2023-09-19 19:21:48 -0500

    Correccion archivos

--- src/main/java/com/procesos/tienda/model/User.java
+++ src/main/java/com/procesos/tienda/model/User.java
@@ -1,8 +1,15 @@
 package com.procesos.tienda.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+
 @Data
+@Entity
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
@@ -11,5 +18,4 @@ public class User {
     private String phone;
     private String email;
     private String password;
-
 }
--- src/main/java/com/procesos/tienda/service/UserService.java
+++ src/main/java/com/procesos/tienda/service/UserService.java
@@ -1,4 +1,45 @@
 package com.procesos.tienda.service;
 
+import com.procesos.tienda.model.User;
+import com.procesos.tienda.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    //Metodo para insertar
+    public User createUser(User userReq){
+        return userRepository.save(userReq);
+    }
+
+    public Optional<User> getUserById(User userReq){
+        return userRepository.findById(userReq.getId());
+    }
+
+    //Metodo para actializar un usuario
+    public User updateUser(User userReq){
+        Optional<User> userBd = userRepository.findById(userReq.getId());
+        if(userBd.isEmpty())
+            return  null;
+        userBd.get().setFirstName(userReq.getFirstName());
+        userBd.get().setLastName(userReq.getLastName());
+        userBd.get().setAddress(userReq.getAddress());
+        userBd.get().setPhone(userReq.getPhone());
+        return userRepository.save(userBd.get());
+    }
+
+    //Metodo para eliminar de una base de datos
+    public boolean deleteUser(User userReq){
+        Optional<User> userBd = userRepository.findById(userReq.getId());
+        if(userBd.isEmpty())
+            return false;
+        userRepository.deleteById(userBd.get().getId());
+        return true;
+    }
+
 }
